020eb473f115a3a10355c9d1172446552df1ff08
Xuangui Huang <xuanguih@nvidia.com>
2025-11-20 13:04:43 -0800

    remove unused dim

--- src/etkdg_stage_distgeom_minimize.cu
+++ src/etkdg_stage_distgeom_minimize.cu
@@ -147,7 +147,6 @@ void DistGeomMinimizeStage::executeImpl(ETKDGContext& ctx,
 
   const size_t numAtoms = ctx.systemHost.atomStarts.back();
   const size_t numPos   = ctx.systemHost.positions.size();
-  const int    dim      = (numPos == numAtoms * 3) ? 3 : 4;
 
   // Allocate energy buffer (not used for DG but required by interface)
   AsyncDeviceVector<double> energyBuffer(0, stream_);
